38a6acfd45f6cd031d48e4228b89bedc0e53c317
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-07-01 22:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function daysOfYear() {
+	if(daysOfYear % 4 === 0)
+		return(366);
+	else{
+		return(365);
+	}
+	
+}
+console.log(daysOfYear(2022));
+function daysOfYear() {
+	if(daysOfYear % 4 === 0)
+		return(366);
+	else{
+		return(355);
+	}
+	
+}
+console.log(daysOfYear(2022))
+console.log(daysOfYear(2024))
+console.log(daysOfYear(1900))
+console.log(daysOfYear(2000))
+
